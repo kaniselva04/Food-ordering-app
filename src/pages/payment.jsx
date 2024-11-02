17a17a999fd1adf610b5721822190b0dde605ebd
C-Sakthi-Maheswari <sakthi2k414@gmail.com>
2024-11-02 21:47:31 +0530

    DONE

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { QRCodeCanvas } from 'qrcode.react'; 
+//import { QRCodeCanvas } from 'qrcode.react'; 
 import '../css/payment.css';
 
 const Payment = () => {
